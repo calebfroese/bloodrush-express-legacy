e2c5c80c3b9f6ed88354c0aa5b9e87306d644600
Caleb Froese <caleb.froese@gmail.com>
2017-01-02 19:17:40 +1030

    Perfected round robbing

--- season/season.js
+++ season/season.js
@@ -29,20 +29,21 @@ module.exports = {
 
                 var playerCount = playersArray.length;
                 
-                if (playerCount % 2 === 1) {
-                    playersArray.push('Bye'); // so we can match algorithm for even numbers
-                    playerCount += 1;
-                }
+                // if (playerCount % 2 === 1) {
+                //     playersArray.push('Bye'); // so we can match algorithm for even numbers
+                //     playerCount += 1;
+                // }
 
                 var games = [];
                 for(var i = 0; i < playerCount - 1; i++) {
                     games[i] = [];
                     for(var j = 0; j < playerCount / 2; j++) {
-                        games[i].push([playersArray[j], playersArray[j + (playerCount/2)]]);
+                        games[i].push([playersArray[j], playersArray[playerCount - j - 1]]);
                     }
                     console.log(`${playersArray[0]} ${playersArray[1]} ${playersArray[2]} ${playersArray[3]}`);
-                    console.log(`${playersArray[4]} ${playersArray[5]} ${playersArray[6]} ${playersArray[7]}`);
+                    console.log(`${playersArray[7]} ${playersArray[6]} ${playersArray[5]} ${playersArray[4]}`);
                     console.log('--');
+
                     // Move the arrays
                     var first = playersArray[0];
                     var last = playersArray[playerCount - 1];
@@ -54,44 +55,6 @@ module.exports = {
                 }
                 console.log(games);
                 callback(games);
-                
-                // const BYE = 'BYE';
-                // var playersArray = [];
-                // teamsArray.forEach(function (team) {
-                //     playersArray.push(team.name);
-                // }, this);
-                // var playerCount = playersArray.length;
-                // var roundRobinArray = [];                  // rs = round array
-                
-                // playersArray = playersArray.slice();
-                
-
-                // if (playerCount % 2 === 1) {
-                //     playersArray.push(BYE); // so we can match algorithm for even numbers
-                //     playerCount += 1;
-                // }
-                // for (var j = 0; j < playerCount - 1; j += 1) {
-                //     roundRobinArray[j] = []; // create inner match array for round j
-                //     for (var i = 0; i < playerCount / 2; i += 1) {
-                //         if (playersArray[i] !== BYE && playersArray[playerCount - 1 - i] !== BYE) {
-                //             roundRobinArray[j].push([playersArray[i], playersArray[playerCount - 1 - i]]); // insert pair as a match
-                //         }
-                //     }
-                //     playersArray.splice(1, 0, playersArray.pop()); // permutate for next round
-                // }
-                
-                // callback(roundRobinArray);
-
-
-                // var teamIdArray = [];
-                // teamsArray.forEach(function (team) {
-                //     teamIdArray.push(team.name);
-                // }, this);
-                // var players = teamIdArray;
-                // var ar = robin(players.length, players);
-
-                // console.log(ar);
-                // callback(ar);
             })
         } else {
             callback('no database!');
