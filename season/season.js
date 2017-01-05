58099097854f3070e3a76cfd1c12b3912a55b047
Caleb Froese <caleb.froese@gmail.com>
2017-01-05 22:28:10 +1030

    Fix up season gen

--- season/season.js
+++ season/season.js
@@ -6,64 +6,19 @@ var moment = require('moment');
 const IDEAL_TOTAL_GAMES_PLAYED = 26; // season matcher will try to get close to this value of games played (therefore days)
 var nextGameDate = moment();
 var roundNumber = 1;
-var createRounds = (playersArray) => {
-    // Logic from: http://stackoverflow.com/questions/6648512/scheduling-algorithm-for-a-round-robin-tournament
-    // Create a round where each player will play everyone else once
-    var playerCount = playersArray.length;
-    // If there is an odd number, add a null (which is a bye)
-    if (playerCount % 2 === 1) {
-        playersArray.push(null);
-        playerCount += 1;
-    }
 
-    var gamesArray = [];
-
-    for (var i = 0; i < playerCount - 1; i++) {
-
-        for (var j = 0; j < playerCount / 2; j++) {
-            gamesArray.push([playersArray[j], playersArray[playerCount - j - 1], moment(nextGameDate).toDate(), roundNumber]);
-            console.log(moment(nextGameDate).format('DD/MM'));
-        }
-        // Move the arrays
-        var first = playersArray[0];
-        var last = playersArray[playerCount - 1];
-
-        playersArray.splice(-1, 1);
-        playersArray.splice(0, 1);
-        playersArray.unshift(last);
-        playersArray.unshift(first);
-
-        roundNumber++;
-        nextGameDate = moment(nextGameDate).add(1, 'day');
-    }
-
-    return gamesArray;
-}
-
-var shuffle = (array) => {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex][0];
-        array[currentIndex][0] = array[randomIndex][0];
-        array[randomIndex][0] = temporaryValue;
-
-        temporaryValue = array[currentIndex][1];
-        array[currentIndex][1] = array[randomIndex][1];
-        array[randomIndex][1] = temporaryValue;
-    }
-    return array;
-}
 
 module.exports = {
     byNumber: (database, params, callback) => {
         // Fetches the season by its number
         database.collection('seasons').find().toArray((err, items) => {
             callback(items);
+        });
+    },
+    byActive: (database, params, callback) => {
+        // Fetches the active season
+        database.collection('seasons').find({ 'active': true }).toArray((err, items) => {
+            callback(items[0]);
         });
     },
     generateSeason: (callback) => {
@@ -86,15 +41,15 @@ module.exports = {
                         teamsIdArray.push(team._id);
                     }, this);
 
-                    var season = [];
-                    season = shuffle(createRounds(teamsIdArray));
-                    var gamesPerSeason = season.length;
+                    var regularGames = [];
+                    regularGames = shuffle(createRounds(teamsIdArray));
+                    var gamesThisSeason = regularGames.length;
 
                     var over;
                     var under;
                     var use;
                     for (var i = 1; i < IDEAL_TOTAL_GAMES_PLAYED; i++) {
-                        if (i * gamesPerSeason > IDEAL_TOTAL_GAMES_PLAYED) {
+                        if (i * gamesThisSeason > IDEAL_TOTAL_GAMES_PLAYED) {
                             over = i;
                             break;
                         } else {
@@ -102,33 +57,133 @@ module.exports = {
                         }
                     }
                     // Should we go over the amount of games, or under, dpeending which is closer
-                    if (IDEAL_TOTAL_GAMES_PLAYED - (under * gamesPerSeason) > (over * gamesPerSeason) - IDEAL_TOTAL_GAMES_PLAYED) {
+                    if (IDEAL_TOTAL_GAMES_PLAYED - (under * gamesThisSeason) > (over * gamesThisSeason) - IDEAL_TOTAL_GAMES_PLAYED) {
                         // Use over
                         use = over;
                     } else {
                         use = under;
                     }
-
                     for (var i = 1; i < use; i++) {
-                        season = season.concat(shuffle(createRounds(teamsIdArray)));
+                        regularGames = regularGames.concat(shuffle(createRounds(teamsIdArray)));
                     }
-
-                    // Save the season
-                    databaseLocalScope.collection('seasons').findOne({ $query: {}, $orderby: { number: -1 } }, results => {
-                        var highestSeason = 1;
-                        if (results) {
-                            console.log(results);
-                        }
-                        console.log({ number: 1, games: season });
+                    // Fetch the season number
+                    var highestSeason = 1;
+                    getHighestSeason(databaseLocalScope).then(array => {
+                        array.forEach(a => {
+                            // Iterate through the seasons to find the highest
+                            if (a.number >= highestSeason) {
+                                highestSeason = a.number + 1;
+                            }
+                        });
+                    }).catch(err => {
+                        console.log('error', err);
+                        return;
+                    }).then(() => {
+                        databaseLocalScope.collection('seasons').update({
+                            active: true
+                        }, { $set: { active: false } }, { upsert: false, multi: true });
+                        console.log('Updating old seasons');
+                    }).then(() => {
+                        // Generate playoffs
+                        var playoffGames = generatePlayoffs();
+                        var fullSeason = regularGames.concat(playoffGames);
+                        // Add season
                         databaseLocalScope.collection('seasons').insert({
                             number: highestSeason,
-                            games: season
+                            games: fullSeason,
+                            active: true
                         });
-                        callback({ number: 1, games: season });
                     });
                 })
-
             }
         });
     }
+}
+
+function createRounds(playersArray) {
+    // Logic from: http://stackoverflow.com/questions/6648512/scheduling-algorithm-for-a-round-robin-tournament
+    // Create a round where each player will play everyone else once
+    var playerCount = playersArray.length;
+    // If there is an odd number, add a null (which is a bye)
+    if (playerCount % 2 === 1) {
+        playersArray.push(null);
+        playerCount += 1;
+    }
+
+    var gamesArray = [];
+
+    for (var i = 0; i < playerCount - 1; i++) {
+
+        for (var j = 0; j < playerCount / 2; j++) {
+            gamesArray.push({
+                'home': playersArray[j],
+                'away': playersArray[playerCount - j - 1],
+                'date': moment(nextGameDate).toDate(),
+                'round': roundNumber,
+                'game': {}
+            });
+        }
+        // Move the arrays
+        var first = playersArray[0];
+        var last = playersArray[playerCount - 1];
+
+        playersArray.splice(-1, 1);
+        playersArray.splice(0, 1);
+        playersArray.unshift(last);
+        playersArray.unshift(first);
+
+        roundNumber++;
+        nextGameDate = moment(nextGameDate).add(1, 'day');
+    }
+
+    return gamesArray;
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+function getHighestSeason(db) {
+    var database = db;
+    return new Promise((resolve, reject) => {
+        database.collection('seasons').find().toArray((err, array) => {
+            if (err) reject(err)
+            resolve(array);
+        });
+    });
+}
+
+function generatePlayoffs() {
+    var gamesArray = [];
+    for (var i = 0; i < 4; i++) {
+        var finalName = ['qtr', 'semi', 'final', 'grand'];
+        var positions = [[1, 8], [2, 7], [3, 6], [4, 5]];
+        gamesArray.push({
+            'home': null,
+            'homeRank': positions[i][0],
+            'away': null,
+            'awayRank': positions[i][1],
+            'date': moment(nextGameDate).toDate(),
+            'round': (finalName[i] + i),
+            'game': {}
+        });
+
+        nextGameDate = moment(nextGameDate).add(1, 'day');
+    }
+    return gamesArray;
 }
